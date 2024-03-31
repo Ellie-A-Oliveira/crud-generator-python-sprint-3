644023ee1f48b9005c1f31642c112da2424d133b
Ellie de Oliveira <ellie.a.oliveira@gmail.com>
2024-03-31 11:48:10 -0300

    feat(SF-91): add all different database models and create a menu for each crud

--- programas/lista-campos.py
+++ programas/lista-campos.py
@@ -2,12 +2,75 @@ from programas.crud import gerar_campo
 from programas.helper import InputTypes
 
 campos_cliente = [
-    gerar_campo("id", "ID", InputTypes.NUMERO_INTEIRO, identificador=True),
-    gerar_campo("nome", "Nome", InputTypes.TEXTO),
-    gerar_campo("sobrenome", "Sobrenome", InputTypes.TEXTO),
-    gerar_campo("email", "E-mail", InputTypes.TEXTO),
-    gerar_campo("tipo", "Tipo", InputTypes.TEXTO),
-    gerar_campo("idioma", "Idioma", InputTypes.TEXTO),
-    gerar_campo("pais", "País", InputTypes.TEXTO),
-    gerar_campo("telefone", "Telefone", InputTypes.TEXTO),
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("nome", "Nome"),
+    gerar_campo("sobrenome", "Sobrenome"),
+    gerar_campo("email", "E-mail"),
+    gerar_campo("tipo", "Tipo"),
+    gerar_campo("idioma", "Idioma"),
+    gerar_campo("pais", "País"),
+    gerar_campo("telefone", "Telefone"),
+]
+
+campos_empresa = [
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("nome", "Nome"),
+    gerar_campo("tipo_industria", "Indústria", artigo="a"),
+    gerar_campo("tamanho", "Tamanho"),
+    gerar_campo("pais_sede", "País Sede"),
+]
+
+campos_funcionario = [
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("nome", "Nome"),
+    gerar_campo("sobrenome", "Sobrenome"),
+    gerar_campo("email", "E-mail"),
+    gerar_campo("cargo", "Cargo"),
+    gerar_campo("telefone", "Telefone"),
+    gerar_campo("salario", "Salário", tipo=InputTypes.NUMERO_REAL),
+]
+
+campos_perguntas_frequentes = [
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("tipo_produto", "Tipo do Produto"),
+    gerar_campo("pergunta", "Pergunta", artigo="a"),
+    gerar_campo("resposta", "Resposta", artigo="a"),
+]
+
+campos_produto = [
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("empresa", "Empresa", artigo="a"),
+    gerar_campo("preco", "Preço", tipo=InputTypes.NUMERO_REAL),
+    gerar_campo("status", "Status"),
+    gerar_campo("plano_contratado", "Plano Contratado"),
+    gerar_campo("tipo_produto", "Tipo do Produto"),
+    gerar_campo("teste_gratis_ate", "Teste Grátis Até"),
+]
+
+campos_recurso = [
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("nome", "Nome"),
+    gerar_campo("notas_preco", "Notas do Preço", artigo="as"),
+    gerar_campo("descricao", "Descrição", artigo="a"),
+    gerar_campo("categoria", "Categoria", artigo="a"),
+]
+
+campos_tipo_plano = [
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("nome", "Nome"),
+    gerar_campo("descricao", "Descrição", artigo="a"),
+    gerar_campo("preco", "Preço", tipo=InputTypes.NUMERO_REAL),
+    gerar_campo("tipo_preco", "Tipo do Preço"),
+    gerar_campo("nivel_plano", "Nível do Plano"),
+    gerar_campo("recursos", "Recursos", artigo="os"),
+    gerar_campo("teste_gratis_disponivel", "Teste Grátis Disponível? (1-SIM;0-NÃO)", tipo=InputTypes.VERDADEIRO_FALSO),
+]
+
+campos_tipo_produto = [
+    gerar_campo("id", "ID", tipo=InputTypes.NUMERO_INTEIRO, identificador=True),
+    gerar_campo("nome", "Nome"),
+    gerar_campo("descricao", "Descrição", artigo="a"),
+    gerar_campo("planos_disponíveis", "Planos Disponíveis", artigo="os"),
+    gerar_campo("is_add_on", "É Add On? (1-SIM;0-NÃO)", tipo=InputTypes.VERDADEIRO_FALSO),
+    gerar_campo("nome_grupo", "Nome do Grupo"),
 ]
--- programas/main.py
+++ programas/main.py
@@ -1,33 +1,84 @@
 from programas.crud import gerar_crud
 from programas.helper import safe_input, InputTypes, deseja_continuar
 
-campos_cliente = getattr(__import__("programas.lista-campos"), "lista-campos").campos_cliente
+campos = getattr(__import__("programas.lista-campos"), "lista-campos")
+
+campos_cliente = campos.campos_cliente
+campos_empresa = campos.campos_empresa
+campos_funcionario = campos.campos_funcionario
+campos_perguntas_frequentes = campos.campos_perguntas_frequentes
+campos_produto = campos.campos_produto
+campos_recurso = campos.campos_recurso
+campos_tipo_plano = campos.campos_tipo_plano
+campos_tipo_produto = campos.campos_tipo_produto
 
 
-def main():
-    cliente_crud = gerar_crud("o", "Cliente", campos_cliente)
+campos_para_crud = [
+    {"campos": campos_cliente, "artigo": "o", "nome": "Cliente"},
+    {"campos": campos_empresa, "artigo": "a", "nome": "Empresa"},
+    {"campos": campos_funcionario, "artigo": "o", "nome": "Funcionário"},
+    {"campos": campos_perguntas_frequentes, "artigo": "as", "nome": "Perguntas Frequentes"},
+    {"campos": campos_produto, "artigo": "o", "nome": "Produto"},
+    {"campos": campos_recurso, "artigo": "o", "nome": "Recurso"},
+    {"campos": campos_tipo_plano, "artigo": "o", "nome": "Tipo do Plano"},
+    {"campos": campos_tipo_produto, "artigo": "o", "nome": "Tipo do Produto"},
+]
 
-    print(f"Bem-vindo ao CRUD para {cliente_crud.nome_classe_dados}!")
+
+def listar_crud(tipo_crud):
+    print(f"Bem-vindo ao CRUD para {tipo_crud.nome_classe_dados}!")
     _continue = 1
     while _continue == 1:
-        print("Opções:\n1-LISTAR\n2-LISTAR UM\n3-CRIAR\n4-ATUALIZAR UM\n5-REMOVER UM\n")
+        print("Opções:\n1-LISTAR\n2-LISTAR UM\n3-CRIAR\n4-ATUALIZAR UM\n5-REMOVER UM\n0-SAIR\n")
         opcao = safe_input("Digite a opção que deseja: ", InputTypes.NUMERO_INTEIRO)
 
         match opcao:
             case 1:
-                cliente_crud.listar()
+                tipo_crud.listar()
             case 2:
-                cliente_crud.listar_pelo_identificador()
+                tipo_crud.listar_pelo_identificador()
             case 3:
-                cliente_crud.criar()
+                tipo_crud.criar()
             case 4:
-                cliente_crud.atualizar_pelo_identificador()
+                tipo_crud.atualizar_pelo_identificador()
             case 5:
-                cliente_crud.remover_pelo_identificador()
+                tipo_crud.remover_pelo_identificador()
+            case 0:
+                break
             case _:
                 print("Opção inválida!")
 
         _continue = deseja_continuar()
+
+
+def main():
+    cruds = [crud for crud in list(map(lambda campo: gerar_crud(campo["artigo"], campo["nome"], campo["campos"]), campos_para_crud))]
+
+    print("Bem-vindo ao programa de CRUDs!")
+
+    _continue = 1
+    while _continue == 1:
+        for i in range(len(cruds)):
+            crud = cruds[i]
+            numero_opcao = i + 1
+            print(f"{numero_opcao}-{crud.nome_classe_dados}")
+
+        opcao = safe_input("\nDigite o CRUD desejado: ", InputTypes.NUMERO_INTEIRO)
+        idx_crud_selecionado = next(
+            filter(
+                lambda idx_campo: idx_campo == opcao - 1,
+                range(len(cruds))
+            ),
+            None
+        )
+
+        if idx_crud_selecionado is not None:
+            crud = cruds[idx_crud_selecionado]
+            listar_crud(crud)
+        else:
+            print("Opção inválida!")
+
+        _continue = deseja_continuar("Deseja continuar no programa de CRUD?")
 
 
 if __name__ == '__main__':
